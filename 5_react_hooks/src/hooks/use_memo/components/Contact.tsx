08f887a4b2840840fe8bf8c3b5e76205fc74f76e
KaanCaman <caman31314@gmail.com>
2024-03-30 22:06:27 +0300

    useMemo hooku öğrenilip pratik yapıldı. / The useMemo hook was learned and practiced.

--- 5_react_hooks/src/hooks/use_memo/components/Contact.tsx
+++ 5_react_hooks/src/hooks/use_memo/components/Contact.tsx
@@ -1,27 +1,77 @@
-const Contact = () => {
+import { memo, useCallback, useReducer, useRef } from "react";
+import reducer from "../resources/contact_reducer";
+import { ConState, ConActionTypes } from "../resources/contact_type";
+import ContactForm from "./ContactForm";
+import ContactList from "./ContactList";
+
+/**
+ *
+ * Contact component
+ * Contact bileşeni
+ *
+ * @returns JSX.Element
+ *
+ * @description
+ * Contact component is used to display the contacts and add a new contact.
+ * Contact bileşeni kişileri göstermek ve yeni kişi eklemek için kullanılır.
+ *
+ *  */
+const Contact = memo(() => {
+  // log the rendering
+  // renderi logla
+
+  console.log("use_memo.components.Contact rendered.");
+
+  // define the initial state
+  // başlangıç state'ini tanımla
+  const initialState: ConState = {
+    contactList: [],
+    contact: "",
+  };
+
+  // define the input reference
+  // input referansını tanımla
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  // define the submit handler
+  // submit handler'ı tanımla
+  const onSubmitHandle = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    //
+    //  1. Check if the input value is not empty.
+    //  2. If it is not empty, dispatch the action to add the contact.
+    //  3. Clear the input value.
+    //  4. Prevent the default form submission.
+    //
+    //  1. Input değerinin boş olmadığını kontrol et.
+    //  2. Boş değilse, kişiyi eklemek için action'u dispatch et.
+    //  3. Input değerini temizle.
+    //  4. Formun default gönderimini engelle.
+    //
+    if (inputRef.current?.value) {
+      dispatch({
+        type: ConActionTypes.ADD_CONTACT,
+        payload: inputRef.current.value,
+      });
+      inputRef.current.value = "";
+    }
+    e.preventDefault();
+  }, []);
+
+  // use the reducer
+  // reducer'ı kullan
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // return the JSX
   return (
     <>
       <div className="contact-area">
-        <form className="form">
-          <input type="text" placeholder="Kişi Ekle" className="form-input" />
-          <button type="submit" className="form-submit">
-            Add / Ekle
-          </button>
-        </form>
-
+        <ContactForm ref={inputRef} onSubmitHandle={onSubmitHandle} />
         <div className="scrollable-area">
-          <ul className="list">
-            {Array.from({ length: 20 }).map((_, index) => (
-              <li key={index} className="list-item">
-                <span className="list-item-text">Kişi {index + 1}</span>
-                <button className="list-item-delete">X</button>
-              </li>
-            ))}
-          </ul>
+          <ContactList contactList={state.contactList} dispatch={dispatch} />
         </div>
       </div>
     </>
   );
-};
+});
 
 export default Contact;
--- 5_react_hooks/src/hooks/use_memo/components/Footer.tsx
+++ 5_react_hooks/src/hooks/use_memo/components/Footer.tsx
@@ -1,11 +1,37 @@
-const Footer = () => {
+import { memo } from "react";
+
+/**
+ *
+ * Footer component
+ * Footer bileşeni
+ *
+ * @returns JSX.Element
+ *
+ * @description
+ * Footer component is used to display the footer of the application.
+ * Footer bileşeni uygulamanın footer'ını göstermek için kullanılır.
+ * */
+const Footer = memo(() => {
+  // log the rendering
   console.log("use_memo.components.Footer rendered.");
 
+  // return the JSX
   return (
     <>
-      <footer>Bu uygulamada kullanılan hooklar aşşağgaki gibidir. </footer>
+      <footer>
+        Bu uygulamada kullanılan hooklar aşşağgaki gibidir.
+        <br />
+        The hooks used in this application are as follows.
+        <ul>
+          <li>useState</li>
+          <li>useEffect</li>
+          <li>useRef</li>
+          <li>useMemo</li>
+          <li>useCallback</li>
+        </ul>
+      </footer>
     </>
   );
-};
+});
 
 export default Footer;
